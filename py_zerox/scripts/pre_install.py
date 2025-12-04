18b310897d920197f3316d3333841639d64f6a91
contextualstudio <contextualstudioai@gmail.com>
2025-12-04 23:09:42 +0100

    Revert "Revert "Revert "Revert "Develop""""

--- py_zerox/scripts/pre_install.py
+++ py_zerox/scripts/pre_install.py
@@ -24,26 +24,7 @@ def install_package(command, package_name):
 
 
 def check_and_install():
-    try:
-        # Check and install Poppler
-        try:
-            run_command("pdftoppm -h")
-        except RuntimeError:
-            if platform.system() == "Darwin":  # macOS
-                install_package("brew install poppler", "Poppler")
-            elif platform.system() == "Linux":  # Linux
-                install_package(
-                    "sudo apt-get update && sudo apt-get install -y poppler-utils",
-                    "Poppler",
-                )
-            else:
-                raise RuntimeError(
-                    "Please install Poppler manually from https://poppler.freedesktop.org/"
-                )
-
-    except RuntimeError as err:
-        print(f"Error during installation: {err}", file=sys.stderr)
-        sys.exit(1)
+    pass
 
 
 if __name__ == "__main__":
